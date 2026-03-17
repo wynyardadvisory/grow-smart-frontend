105d03c6b1120ab32465daba85ec1fc0d901b466
Mark <mark@wynyardadvisory.co.uk>
2026-03-17 00:38:07 +0000

    feat: PWA icons and manifest for home screen install

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Vercro" />
+        <meta name="theme-color" content="#2F5D50" />
+        <meta name="mobile-web-app-capable" content="yes" />
+      </Head>
       <body>
         <Main />
         <NextScript />
