170ed2c4d067584fe5b96a5754030fb4a802eda6
Mark <mark@wynyardadvisory.co.uk>
2026-05-06 17:56:51 +0100

    Add PostHog initialisation

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
 import "@/styles/globals.css";
+import posthog from "posthog-js";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      defaults: "2026-01-30",
+      loaded: (ph) => {
+        if (process.env.NODE_ENV === "development") ph.debug();
+      },
+    });
+
+    const handleRouteChange = () => posthog.capture("$pageview");
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, []);
+
   return <Component {...pageProps} />;
 }
